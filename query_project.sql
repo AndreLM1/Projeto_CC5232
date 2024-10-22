d64ce5ff526c6a0fba90f23716574abfd3dd89a1
André Loureço Marques <118229460+AndreLM1@users.noreply.github.com>
2024-10-22 19:00:54 -0300

    Update query_project.sql

--- query_project.sql
+++ query_project.sql
@@ -1,4 +1,5 @@
-use project_spotify;
+CREATE DATABASE  IF NOT EXISTS `project_spotify` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `project_spotify`;
 
 -- Criação das tabelas
  CREATE TABLE ARTISTA (
